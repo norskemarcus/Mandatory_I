ce06faca51369be821ab599364ab78df0dad1732
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-09-26 19:00:31 +0200

    Dummy login works with logged in as: email and logout button shows up when logged in

--- public/frontpage.js
+++ public/frontpage.js
@@ -1,4 +1,10 @@
+// event listener to the Sign In button
+const signInButton = document.getElementById('signInModalButton');
+signInButton.addEventListener('click', handleSignIn);
 
+// event listener to the Sign Out button
+const signOutButton = document.getElementById('signOutButton');
+signOutButton.addEventListener('click', handleSignOut);
 
 
 
@@ -7,35 +13,54 @@ function handleSignIn() {
   const password = document.getElementById('password').value;
 
   // Perform authentication (dummy logic for demonstration)
-  if (email === 'test@example.com' && password === 'test') {
-      showMessage('success', 'Login successful!');
+  if (email === "test1@gmail.com" && password === "test1234") {
+    showMessage('success', 'Login successful!', 'modalMessageDiv');
+    console.log('Login successful!');
+    updateStatus('Logged in as: ' + email);
+    document.getElementById('signInButton').style.display = 'none'; // Hide Sign In button
+    document.getElementById('loggedInStatus').style.display = 'block'; // Show status and Sign Out button
+     // Hide the modal after successful login
+     $('#signInModal').modal('hide');
   } else {
-      showMessage('error', 'Invalid email or password. Please try again.');
+    showMessage('error', 'Invalid email or password. Please try again.', 'modalMessageDiv');
+    console.log('Invalid email or password');
   }
 }
 
-// Function to display messages in the message div
-function showMessage(type, message) {
-  const messageDiv = document.getElementById('messageDiv');
+function showMessage(type, message, targetElementId) {
+  const messageDiv = document.getElementById(targetElementId);
   messageDiv.textContent = message;
 
   if (type === 'success') {
-      messageDiv.style.backgroundColor = '#d4edda';
-      messageDiv.style.borderColor = '#c3e6cb';
-      messageDiv.style.color = '#155724';
+    messageDiv.style.backgroundColor = '#d4edda';
+    messageDiv.style.borderColor = '#c3e6cb';
+    messageDiv.style.color = '#155724';
   } else if (type === 'error') {
-      messageDiv.style.backgroundColor = '#f8d7da';
-      messageDiv.style.borderColor = '#f5c6cb';
-      messageDiv.style.color = '#721c24';
+    messageDiv.style.backgroundColor = '#f8d7da';
+    messageDiv.style.borderColor = '#f5c6cb';
+    messageDiv.style.color = '#721c24';
   }
 
-  messageDiv.style.display = 'block'; 
+  messageDiv.style.display = 'block';
 }
 
-// Add a click event listener to the Sign In button
-const signInButton = document.getElementById('signInButton');
-signInButton.addEventListener('click', handleSignIn);
 
+function updateStatus(statusText) {
+  const statusElement = document.getElementById('status');
+  statusElement.textContent = statusText;
+  document.getElementById('loggedInStatus').style.display = 'block'; // Display the status div
+}
+
+
+function handleSignOut() {
+  // Implement sign-out logic here
+  // This is where you would clear the user's session or token
+  // and reset the UI to the signed-out state
+  document.getElementById('signInButton').style.display = 'block'; // Show Sign In button
+  document.getElementById('loggedInStatus').style.display = 'none'; // Hide status and Sign Out button
+  document.getElementById('status').textContent = ''; // Clear the status text
+  showMessage('success', 'Logged out successfully!');
+}
 
 
 /* 
