37e3224fe92aeb992496ea2f0746687910afffd5
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-10-06 22:09:11 +0200

    removed comments

--- public/pages/frontpage/frontpage.js
+++ public/pages/frontpage/frontpage.js
@@ -1,18 +1,15 @@
-// event listener to the Sign In button
+
 const signInButton = document.getElementById('signInModalButton');
 signInButton.addEventListener('click', handleSignIn);
 
-// event listener to the Sign Out button
 const signOutButton = document.getElementById('signOutButton');
 signOutButton.addEventListener('click', handleSignOut);
 
 
-// Event listener to detect when the modal is hidden
 $('#signInModal').on('hidden.bs.modal', function () {
    document.getElementById('email').value = '';
    document.getElementById('password').value = '';
    
-  // Clear the modal message when the modal is hidden
   const modalMessageDiv = document.getElementById('modalMessageDiv');
   if (modalMessageDiv) {
     showMessage('reset', '', 'modalMessageDiv', true); 
@@ -24,16 +21,15 @@ function handleSignIn() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  // Perform authentication (dummy logic for demonstration)
+  // dummy logic for demonstration
   if (email === "test1@gmail.com" && password === "test1234") {
 
     showMessage('success', 'Login successful!', 'modalMessageDiv');
     console.log('Login successful!');
     updateStatus('Logged in as: ' + email);
-    document.getElementById('signInButton').style.display = 'none'; // Hide Sign In button
-    document.getElementById('loggedInStatus').style.display = 'block'; // Show status and Sign Out button
+    document.getElementById('signInButton').style.display = 'none'; 
+    document.getElementById('loggedInStatus').style.display = 'block'; 
     
-     // Hide the modal after successful login
      $('#signInModal').modal('hide');
   } else {
     showMessage('error', 'Invalid email or password. Please try again.', 'modalMessageDiv');
@@ -76,13 +72,13 @@ function showMessage(type, message, targetElementId, reset = false) {
 function updateStatus(statusText) {
   const statusElement = document.getElementById('status');
   statusElement.textContent = statusText;
-  document.getElementById('loggedInStatus').style.display = 'block'; // Display the status div
+  document.getElementById('loggedInStatus').style.display = 'block'; 
 }
 
 
 function handleSignOut() {
-  // Implement sign-out logic here
-  // This is where you would clear the user's session or token
+  //  sign-out logic 
+  //  clear the user's session or token
   document.getElementById('signInButton').style.display = 'block'; 
   document.getElementById('loggedInStatus').style.display = 'none'; 
 
@@ -106,16 +102,14 @@ function calculateDaysUntil(targetDate) {
   return daysDifference;
 }
 
-// Dato for 1. december (måneder i JavaScript er 0-indekseret, så november er 10)
+// måneder i JavaScript er 0-indekseret, så november er 10
 const dec1Date = new Date(new Date().getFullYear(), 10, 1);
 
-// Dato for julaften
 const christmasDate = new Date(new Date().getFullYear(), 11, 24);
 
-// Opdater antallet af dage til 1. december
 const daysUntilDec1 = calculateDaysUntil(dec1Date);
+
 document.getElementById('countdown-to-dec1').textContent = `Dage til 1. december: ${daysUntilDec1}`;
 
-// Opdater antallet af dage til julaften
 const daysUntilChristmas = calculateDaysUntil(christmasDate);
 document.getElementById('countdown-to-christmas').textContent = `Dage til julaften: ${daysUntilChristmas}`;
